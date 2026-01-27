b9509b38510476be8932271feb5b10591e1d3f29
mid1224 <61130938+mid1224@users.noreply.github.com>
2026-01-27 10:05:40 +0700

    Add folder to store trained models

--- model_train.py
+++ model_train.py
@@ -14,14 +14,17 @@ import fasttext
 model = fasttext.train_supervised(
     input="dataset/dataset.train",
     lr=0.5,             # Lower learning rate is often better for OVA
-    epoch=25,
-    wordNgrams=2,
-    bucket=200000,
-    dim=300,
-    loss='ova',          # Enable multi-label loss
-    pretrainedVectors="pretrained_vectors/cc.vi.300.vec"
+    epoch=25,           # The number of times each examples is seen
+    wordNgrams=2,       # N-gram
+    bucket=200000,      # allocate space for 200000 unique vectors for the n-grams.
+                        # A larger bucket size allows for more distinct n-gram representations (fewer collisions) 
+                        # but increases the size of the .bin model file.
+    dim=300,            # Vector embedding dimension
+    loss='ova',         # Use one vs all (ova) loss
+    # pretrainedVectors="pretrained_vectors/cc.vi.300.vec" # Minimal improvement
 )
-# Precision: 0.79
+# Precision: 0.79 (old dataset with 784 samples)
+# Precision: 0.835 (new dataset with 2103 samples)
 
 # Save the model
 model.save_model("trained_models/model_1.bin")
